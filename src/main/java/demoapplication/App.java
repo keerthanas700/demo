b5c2cb4015b10c868282376a2355bd4c371604aa
keerthanas700 <KeerthS@eu.trimblecorp.net>
2022-08-23 15:49:09 +0530

    sample

---
+++ src/main/java/demoapplication/App.java
@@ -0,0 +1,13 @@
+package demoapplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/demoapplication/test.java
@@ -0,0 +1,19 @@
+package demoapplication;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class test {
+    public static void main(String[] args) throws InterruptedException
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(1000);
+        driver.close();
+    }
+
+    
+}
